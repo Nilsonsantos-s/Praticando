684a4c3bd5a5562bb1d825d4c0a4c47b68223ac9
nilsonsantos-s <nilsonc.s@outlook.com>
2021-09-03 13:49:14 -0300

    exercício

---
+++ Curso em Vídeo/Um print especial/Um print especial.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tamanho = len(msg)
+    print('~'*(tamanho+5))
+    print(' '*1, msg)
+    print('~' * (tamanho+5))
+
+#Programa Principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no Youtube')
+escreva('CeV')
