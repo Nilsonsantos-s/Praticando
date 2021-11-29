1a7d2a14ff65484b59a1ed26f2ca5b1bc754ff36
nilsonsantos-s <nilsonc.s@outlook.com>
2021-11-29 13:21:01 -0300

    Adição do exercício "Par ou Impar"

---
+++ Python/Exercícios Gerais/Bibliotecas/tkinter/Par ou Ímpar/par_ou_impar.py
@@ -0,0 +1,34 @@
+"""
+Par ou Ímpar
+"""
+from tkinter import Tk, Label, Button, Entry, StringVar
+app = Tk()
+app.title('Par ou Ímpar')
+app.geometry('400x200')
+app.configure(bg='brown')
+
+
+def par_ou_impar():
+    """
+    :return: Recebe a entrada.
+             Se resto do número divido por 2 for 0, ele vai criar a Label com o texto "Par".
+             Do contrário, ele cria a Label com o texto "Ímpar".
+    """
+    numero = int(entrada.get())
+    if numero % 2 == 0:
+        resultado.set('Par')
+    else:
+        resultado.set('Ímpar')
+
+Label(text='Número', bg='white', fg='black', relief='groove').pack(ipadx=0, ipady=0)
+
+entrada = Entry(app)
+entrada.pack(ipadx=0, ipady=0, pady=10)
+Button(text='Verificar', command=par_ou_impar,
+       relief='groove', cursor='dot').pack(ipadx=0, ipady=0, padx=10, pady=10)
+resultado = StringVar()
+label_da_resposta = Label(textvariable=resultado, bg='white', fg='black',
+                          font='Arial 15', relief='groove')
+label_da_resposta.pack(ipadx=20, ipady=0, padx=10, pady=30)
+
+app.mainloop()
---
+++ Python/Exercícios Gerais/Bibliotecas/tkinter/Par ou Ímpar/readme.md
@@ -0,0 +1,9 @@
+<h2>Par ou Ímpar:</h2>
+
+Crie um programa que contenha:
+
+2 labels, 1 entrada e 1 botão 
+
+<p>O usuário digitará o número, apertará no botão que, por sua vez, vai retornar uma label dizendo se o número é par ou ímpar.</p>
+
+<p>OBS: Para posicionar todos os objetos, utilize o método pack. Faça com que o texto da label que mostra o resultado seja alterada de acordo com a condição.</p>
